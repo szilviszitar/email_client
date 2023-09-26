b66acf66a993786d1014a13eda7895674ccc4d75
szilviszitar <szilviszitar@gmail.com>
2023-09-26 20:51:16 +0200

    not-found component, email index style

--- src/app/inbox/email-resolver.service.ts
+++ src/app/inbox/email-resolver.service.ts
@@ -1,13 +1,23 @@
-import { ResolveFn, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { ResolveFn, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { EmailService } from './email.service';
 import { inject } from '@angular/core';
 import { Email } from './email';
+import { EMPTY, catchError } from 'rxjs';
 
  
  
 export const EmailResolverService: ResolveFn<Email> =
     (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-          return  inject(EmailService).getEmail(route.paramMap.get('id')!);
+        const router= inject(Router);
+          return  inject(EmailService).getEmail(route.paramMap.get('id')!)
+          .pipe(
+            catchError(()=>{
+                router.navigateByUrl("/inbox/not-found");
+                return EMPTY
+
+               
+            })
+          );
       
     };
 
